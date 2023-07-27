f5f03eede3b305af96b791f2f0c514cf8c9bc0e7
tuszmak <daniel.pinter02@edu.bme.hu>
2023-07-27 10:20:47 +0200

    feat: make connection to mySQL server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+let mysql = require('mysql2');
 const app = express();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'asd13512',
+  insecureAuth: true
+})
 const port = 3000;
 
 app.post("/", (req, res) => {
   res.send("Hello world!");
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
 });
 
 app.listen(port, () => {
